de57dde61b837524411097b675ae7254c570059b
yazshrilld <yazshrilld@gmail.com>
2023-09-13 09:51:18 +0100

    completed sign-in UI and started the dashboard UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-
+import { useMediaQuery } from "@mui/material";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AuthLayout from "./layout/AuthLayout";
+import MobileDeviceNotAllowed from "components/MobileDeviceNotAllowed";
+import "./index.css";
+import SignIn from "pages/Auth/SignIn";
 
 function App() {
-  return (
-    <>
-      <AuthLayout />
-    </>
+  const matches = useMediaQuery("(min-width:600px)");
+
+  return matches ? (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Navigate to="/auth/sign-in" replace />} />
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="sign-in" element={<SignIn />} />
+        </Route>
+      </Routes>
+    </div>
+  ) : (
+    <MobileDeviceNotAllowed />
   );
 }
 
 export default App;
-
-//
--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -1,13 +1,82 @@
-import { Link } from "react-router-dom"
-import { useFormik } from "formik"
-import { useState, useEffect } from "react"
+import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import { useState, useEffect } from "react";
 import { ReactComponent as EyeOpenIcon } from "assets/svg/eye-open.svg";
 import { ReactComponent as EyeClosedIcon } from "assets/svg/eye-closed.svg";
+import InputFormField from "components/InputFormFIeld/InputFormField";
+import BaseButton from "components/BaseButton/BaseButton";
 
 const SignIn = () => {
-  return (
-    <><h1>My Sign In for FX Blotal</h1></>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
 
-export default SignIn
+  const handleShowPasswordVisibility = () => {
+    setShowPassword((prevS) => !prevS);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      userName: "",
+      password: "",
+    },
+    onSubmit: "",
+    validationSchema: "",
+  })
+
+  return (
+    <>
+      <p className="text-3xl mb-1 font-bold">Login</p>
+      <p className="text-[#C2C2C2] text-sm mb-6">
+        Enter your valid login credentials
+      </p>
+      <form className="space-y-7">
+        <InputFormField
+          label="Username"
+          placeholder="Username"
+          name="userName"
+          value={formik.values.userName}
+          type="text"
+          onChange={formik.handleChange}
+          touched={formik.touched.userName}
+          error={formik.errors.userName}
+        />
+
+        <InputFormField
+          label="Password"
+          placeholder="Password"
+          name="password"
+          value={formik.values.password}
+          type={showPassword ? "text" : "password"}
+          onChange={formik.handleChange}
+          touched={formik.touched.password}
+          error={formik.errors.password}
+          appendIcon={
+            showPassword ? (
+              <EyeOpenIcon
+                className="absolute right-2 cursor-pointer w-5 h-5 select-none"
+                onClick={handleShowPasswordVisibility}
+              />
+            ) : (
+              <EyeClosedIcon
+                className="absolute right-2 cursor-pointer w-5 h-5 select-none"
+                onClick={handleShowPasswordVisibility}
+              />
+            )
+          }
+        />
+
+        
+
+        <BaseButton
+          type="submit"
+          // isLoading={PIsLoading || SIsLoading}
+          isLoading={false}
+          customStyle="w-full "
+        >
+          Login
+        </BaseButton>
+      </form>
+    </>
+  );
+};
+
+export default SignIn;
